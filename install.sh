22370fcdb8948d0d8eb275d85f37872b81231658
res876ttb <someone10851222@gmail.com>
2019-01-29 11:44:30 +0800

    Update installation script

--- install.sh
+++ install.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
+# backup original tmux configuration
+mv $HOME/.tmux.conf .tmux.conf.backup
+rm -f $HOME/.tmux.conf.func
+
+# clone from github
 git clone https://github.com/res876ttb/Gold-Tmux.git
 cd Gold-Tmux
+
+# creaet symbolic link to $HOME
 ln -s $PWD/.tmux.conf* $HOME/
+
 
